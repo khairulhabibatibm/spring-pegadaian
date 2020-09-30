41ba173d55ccd183279e10471f584fe7469f49ba
Khairul Habib <mkhairulhabibm@gmail.com>
2020-09-30 16:14:56 +0700

    change development mode

--- src/main/java/com/example/springpegadaian/MyController.java
+++ src/main/java/com/example/springpegadaian/MyController.java
@@ -8,7 +8,7 @@ public class MyController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello World from Development Mode";
+        return "Hello World from Development Mode.";
     }
     
 }
